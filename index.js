2ee4a8e1d4799b59a2b6dd35bc818e6f7281cb1e
Anastasiia bc2026-3 <Anastasiia.Russu@lnu.edu.ua>
2026-03-18 22:20:44 +0200

    Added JSONL parsing, mpg filtering, and cylinder display

--- index.js
+++ index.js
@@ -16,24 +16,51 @@ program.configureOutput({
 program
   .requiredOption("-i, --input <path>", "шлях до вхідного файлу")
   .option("-o, --output <path>", "шлях до файлу для запису")
-  .option("-d, --display", "вивести результат у консоль");
+  .option("-d, --display", "вивести результат у консоль")
+  .option("-c, --cylinders", "відображати кількість циліндрів")
+  .option("-m, --mpg <value>", "фільтр за паливною економністю (нижче заданої)");
 
 program.parse(process.argv);
 const options = program.opts();
 
-let resultData = ""; 
+let rawData = "";
 try {
-  const rawData = fs.readFileSync(options.input, "utf8");
-  resultData = rawData; 
+  rawData = fs.readFileSync(options.input, "utf8");
 } catch (err) {
   console.error("Cannot find input file");
   process.exit(1);
 }
 
-if (options.display) {
-  console.log(resultData);
+let cars = [];
+try {
+  cars = rawData.split("\n").filter(line => line.trim()).map(line => JSON.parse(line));
+} catch (err) {
+  console.error("Invalid JSON format");
+  process.exit(1);
 }
 
-if (options.output) {
+if (options.mpg) {
+  const threshold = parseFloat(options.mpg);
+  cars = cars.filter(car => car.mpg < threshold);
+}
+
+const resultLines = cars.map(car => {
+  let line = car.model;
+  
+  if (options.cylinders) {
+    line += ` ${car.cyl}`;
+  }
+  
+  line += ` ${car.mpg}`;
+  return line;
+});
+
+const resultData = resultLines.join("\n");
+
+if (options.display) {
+  if (resultData) console.log(resultData);
+}
+
+if (options.output && resultData) {
   fs.writeFileSync(options.output, resultData, "utf8");
 }
